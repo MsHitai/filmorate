0887e237f709abe03461ee10e69b4383dfc3b0b2
MsHitai <mmalafeeva@yandex.ru>
2023-04-21 14:01:48 +0300

    style: исправить codestyle

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -35,8 +35,9 @@ public class FilmController {
     @PutMapping()
     public Film updateFilm(@Valid @RequestBody Film film) {
         log.debug("Получен запрос PUT на обновление фильма {}", film.toString());
-        if(!films.containsKey(film.getId())) {
+        if (!films.containsKey(film.getId())) {
             throw new ValidationException("Фильма с таким идентификатором нет в базе");
+
         }
         films.put(film.getId(), film);
 
